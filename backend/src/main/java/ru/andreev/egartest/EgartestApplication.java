27a8cb8bdaf52420aa6c634e4f436fa814aae3d7
Denis <denis.andreevir@gmail.com>
2021-03-03 13:04:55 +0300

    Initialized project

---
+++ backend/src/main/java/ru/andreev/egartest/EgartestApplication.java
@@ -0,0 +1,13 @@
+package ru.andreev.egartest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EgartestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EgartestApplication.class, args);
+    }
+
+}
---
+++ backend/src/test/java/ru/andreev/egartest/EgartestApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.andreev.egartest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EgartestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
