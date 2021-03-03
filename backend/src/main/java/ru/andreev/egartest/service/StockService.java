5ed0b550236419976e47b4d1bd71467ab65d3ec6
Denis <denis.andreevir@gmail.com>
2021-03-03 20:56:29 +0300

    Added stock service

---
+++ backend/src/main/java/ru/andreev/egartest/service/StockService.java
@@ -0,0 +1,17 @@
+package ru.andreev.egartest.service;
+
+import org.springframework.http.ResponseEntity;
+import ru.andreev.egartest.domain.dto.StockDto;
+import java.util.List;
+
+public interface StockService {
+
+    ResponseEntity<?> findAll();
+
+    ResponseEntity<?> save(StockDto stockDto);
+
+    ResponseEntity<?> update(Long id, StockDto stockDto);
+
+    ResponseEntity<?> delete(Long id);
+
+}
